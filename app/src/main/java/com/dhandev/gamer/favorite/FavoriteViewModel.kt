5dc60a67cdb359dd74aa0aabd16b219fd2a2b635
Dhanfinix <66616896+Dhanfinix@users.noreply.github.com>
2022-11-18 10:02:56 +0700

    penyesuaian awal

--- app/src/main/java/com/dhandev/gamer/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/dhandev/gamer/favorite/FavoriteViewModel.kt
@@ -1,13 +1,13 @@
-package com.dhandev.gamer.ui.home
+package com.dhandev.gamer.favorite
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class FavoriteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is favorite Fragment"
     }
     val text: LiveData<String> = _text
 }

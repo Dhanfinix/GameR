853f7420f0fd2b338c3feca9588611d409559d47
Dhanfinix <66616896+Dhanfinix@users.noreply.github.com>
2022-11-29 16:58:46 +0700

    CI, LeakCanary, Obfuscation, Encryption, Certificate Pinning

--- core/src/main/java/com/dhandev/gamer/core/di/CoreModule.kt
+++ core/src/main/java/com/dhandev/gamer/core/di/CoreModule.kt
@@ -7,6 +7,9 @@ import com.dhandev.gamer.core.data.source.remote.RemoteDataSource
 import com.dhandev.gamer.core.data.source.remote.network.ApiService
 import com.dhandev.gamer.core.domain.repository.IGamesRepository
 import com.dhandev.gamer.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -18,10 +21,14 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module{
     factory { get<GamesDatabase>().gamesDao() }
     single {
+        val passPhrase : ByteArray = SQLiteDatabase.getBytes("dhandev".toCharArray())
+        val factory = SupportFactory(passPhrase)
         Room.databaseBuilder(
             androidContext(),
             GamesDatabase::class.java, "Games.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
@@ -29,10 +36,17 @@ val loggingInterceptor = if(BuildConfig.DEBUG) { HttpLoggingInterceptor().setLev
 
 val networkModule = module {
     single {
+        val hostname = "api.rawg.io"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/6i+nf58l8neEnNarZOvxiYfVbt2S2xurswGQQBBMa0U=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .add(hostname, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
